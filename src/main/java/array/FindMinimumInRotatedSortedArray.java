f7c67ac96b7954e521b8c9200913f3f75bdede80
Yishen <844147804@qq.com>
2021-04-01 14:22:15 +0800

    添加

---
+++ src/main/java/array/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,37 @@
+package array;
+
+/**
+ * <pre>
+ * https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/。
+ * </pre>
+ *
+ * @author Yishen 844147804@qq.com
+ * @version 1.00.00
+ */
+public class FindMinimumInRotatedSortedArray {
+    public int findMin(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int pivot = low + (high - low) / 2;
+            if (nums[pivot] < nums[high]) {
+                // 如果nums[pivot] < nums[high]，则证明pivot在右侧数组上面
+                high = pivot;
+            } else if (nums[pivot] > nums[high]) {
+                // 如果nums[pivot] > nums[high]，则证明pivot在左侧数组上面
+                low = pivot + 1;
+            } else {
+                // 如果nums[pivot] = nums[high]，则nums[pivot] <= nums[high - 1]
+                high -= 1;
+            }
+        }
+        return nums[low];
+    }
+}
